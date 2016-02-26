80abe745bbc826976841122b20c016dcb8f626e3
Amy Madden <amymadden@Amys-iMac.local>
2016-02-26 09:43:13 -0500

    change key generator to key management

--- documentation/templates/partials/navbar_content.php
+++ documentation/templates/partials/navbar_content.php
@@ -2,5 +2,5 @@
 
 <a class="brand navbar-brand pull-right" href="https://lyric-demo-server.herokuapp.com/docs/assignments-api/">Assignment API</a>
 <a class="brand navbar-brand pull-right" href="https://integrationservices.lyricfinancial.com/docs/vendor-api/">Vendor API</a>
-<a class="brand navbar-brand pull-right" href="https://integrationservices.lyricfinancial.com/key-generator/#/keys">Key Generation</a>
+<a class="brand navbar-brand pull-right" href="https://integrationservices.lyricfinancial.com/key-management/#/keys">Key Management</a>
 <a class="brand navbar-brand pull-right" href="http://dev-docs.lyricfinancial.com/Getting_Started.html">Documentation</a>
