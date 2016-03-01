6f15eeea1dc0e1978a35d5e0423936e8db57932e
Amy Madden <amymadden@Amys-iMac.local>
2016-03-01 12:04:43 -0500

    changed assignment api url in docs

--- documentation/templates/partials/navbar_content.php
+++ documentation/templates/partials/navbar_content.php
@@ -1,6 +1,6 @@
 <a class="brand navbar-brand pull-left" href="<?= $params['base_page'] . $params['index']->getUri(); ?>"><?= $params['title']; ?></a>
 
-<a class="brand navbar-brand pull-right" href="https://lyric-demo-server.herokuapp.com/docs/assignments-api/">Assignment API</a>
+<a class="brand navbar-brand pull-right" href="https://lyric-demo-dev-server.herokuapp.com/docs/assignments-api/">Assignment API</a>
 <a class="brand navbar-brand pull-right" href="https://integrationservices.lyricfinancial.com/docs/vendor-api/">Vendor API</a>
 <a class="brand navbar-brand pull-right" href="https://integrationservices.lyricfinancial.com/key-management/#/keys">Key Management</a>
 <a class="brand navbar-brand pull-right" href="http://dev-docs.lyricfinancial.com/Getting_Started.html">Documentation</a>
