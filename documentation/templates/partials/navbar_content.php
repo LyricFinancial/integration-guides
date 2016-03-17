4d50a9afc1aa5b4be4ab716d955df8272ef6ae86
Amy Madden <amymadden@Amys-iMac.local>
2016-03-17 10:33:41 -0400

    added nav header to demos

--- documentation/templates/partials/navbar_content.php
+++ documentation/templates/partials/navbar_content.php
@@ -1,6 +1,7 @@
 <a class="brand navbar-brand pull-left" href="<?= $params['base_page'] . $params['index']->getUri(); ?>"><?= $params['title']; ?></a>
 
-<a class="brand navbar-brand pull-right" href="https://lyric-demo-dev-server.herokuapp.com/docs/assignments-api/">Assignment API</a>
-<a class="brand navbar-brand pull-right" href="https://integrationservices.lyricfinancial.com/docs/vendor-api/">Vendor API</a>
-<a class="brand navbar-brand pull-right" href="https://integrationservices.lyricfinancial.com/key-management/#/keys">Key Management</a>
-<a class="brand navbar-brand pull-right" href="http://dev-docs.lyricfinancial.com/Getting_Started.html">Documentation</a>
+<a class="brand navbar-brand pull-right" href="http://vatm-demo.lyricfinancial.com/#/demo-server">Demos</a>
+<a class="brand navbar-brand pull-right" href="https://lyric-demo-server.herokuapp.com/docs/assignments-api/">Assignment API</a>
+<a class="brand navbar-brand pull-right" href="https://demoservices.lyricfinancial.com/docs/vendor-api/">Vendor API</a>
+<a class="brand navbar-brand pull-right" href="https://demoservices.lyricfinancial.com/key-management/#/keys">Key Management</a>
+<a class="brand navbar-brand pull-right" href="http://docs.lyricfinancial.com/Getting_Started.html">Documentation</a>
