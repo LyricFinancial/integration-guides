f6a646427810c8ef468b8fb63d03ef4e1f3769a9
Amy Madden <amymadden@Amys-iMac-3.local>
2016-05-27 15:31:20 -0400

    add assignments to nav bar

--- documentation/templates/partials/navbar_content.php
+++ documentation/templates/partials/navbar_content.php
@@ -3,5 +3,6 @@
 <a class="brand navbar-brand pull-right" href="http://client-demo-stage.lyricfinancial.com/#/demo-server">Demos</a>
 <a class="brand navbar-brand pull-right" href="/secure/assignments-api/">Assignment API</a>
 <a class="brand navbar-brand pull-right" href="/secure/vendor-api/">Vendor API</a>
+<a class="brand navbar-brand pull-right" href="/secure/assignments/#/advances">Assignments</a>
 <a class="brand navbar-brand pull-right" href="/secure/settings/#/settings">Settings</a>
 <a class="brand navbar-brand pull-right" href="/secure/docs/Getting_Started.html">Documentation</a>
