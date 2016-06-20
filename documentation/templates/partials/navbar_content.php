e33eaf1b5abb6d9263f1488fabee99e17006e089
Amy Madden <amymadden@Amys-iMac-3.local>
2016-06-20 11:32:24 -0400

    added logout functionality to documentation app

--- documentation/templates/partials/navbar_content.php
+++ documentation/templates/partials/navbar_content.php
@@ -1,8 +1,22 @@
 <a class="brand navbar-brand pull-left" href="<?= $params['base_page'] . $params['index']->getUri(); ?>"><?= $params['title']; ?></a>
 
+<a class="brand navbar-brand pull-right" href="#" onclick="logout()">Logout</a>
 <a class="brand navbar-brand pull-right" href="http://client-demo-stage.lyricfinancial.com/#/demo-server">Demos</a>
 <a class="brand navbar-brand pull-right" href="/secure/assignments-api/">Assignment API</a>
 <a class="brand navbar-brand pull-right" href="/secure/vendor-api/">Vendor API</a>
 <a class="brand navbar-brand pull-right" href="/secure/assignments/#/advances">Assignments</a>
 <a class="brand navbar-brand pull-right" href="/secure/settings/#/settings">Settings</a>
 <a class="brand navbar-brand pull-right" href="/secure/docs/Getting_Started.html">Documentation</a>
+
+<script type="text/javascript">
+
+  function logout() {
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.open("GET", "/logout", false);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send();
+    window.location.replace("/login")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     ;
+  }
+
+</script>
