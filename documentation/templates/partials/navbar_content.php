4365a96b99e37b1ce21dd9a885fb75cbeb6e30f8
Amy Madden <amymadden@Amys-iMac-3.local>
2016-07-12 09:13:30 -0400

    update demo to not include portal links

--- documentation/templates/partials/navbar_content.php
+++ documentation/templates/partials/navbar_content.php
@@ -1,10 +1,10 @@
 <a class="brand navbar-brand pull-left" href="<?= $params['base_page'] . $params['index']->getUri(); ?>"><?= $params['title']; ?></a>
 
 <a class="brand navbar-brand pull-right" href="#" onclick="logout()">Logout</a>
-<a class="brand navbar-brand pull-right" href="http://client-demo-stage.lyricfinancial.com/#/demo-server">Demos</a>
+<a class="brand navbar-brand pull-right" href="http://client-demo-stage.lyricfinancial.com/#/demo-server" target='_blank'>Demos</a>
 <a class="brand navbar-brand pull-right" href="/secure/assignments-api/">Assignment API</a>
 <a class="brand navbar-brand pull-right" href="/secure/vendor-api/">Vendor API</a>
-<a class="brand navbar-brand pull-right" href="/secure/assignments/#/advances">Assignments</a>
+<a class="brand navbar-brand pull-right" href="/secure/assignments/#/advances">Advance Approvals</a>
 <a class="brand navbar-brand pull-right" href="/secure/settings/#/settings">Settings</a>
 <a class="brand navbar-brand pull-right" href="/secure/docs/Getting_Started.html">Documentation</a>
 
