7884cb4253684c96db77e5b802897b5d73b835b6
sclancy-70 <clancyconsultant@gmail.com>
2022-02-11 04:16:45 +0000

    update link

--- documentation/templates/partials/navbar_content.php
+++ documentation/templates/partials/navbar_content.php
@@ -5,7 +5,7 @@
 <a class="brand navbar-brand pull-right" href="#" onclick="logout()">Logout</a>
 <a class="brand navbar-brand pull-right" href="http://client-demo-stage.lyricfinancial.com/#/demo-server" target='_blank'>Demos</a>
 <a class="brand navbar-brand pull-right" href="/secure/assignments-api/">Assignment API</a>
-<a class="brand navbar-brand pull-right" href="/secure/vendor-api/">Vendor API</a>
+<a class="brand navbar-brand pull-right" href="http://18.207.204.223:8080">Partner API</a>
 <a class="brand navbar-brand pull-right" href="/secure/finance/#/advances">Finance</a>
 <a class="brand navbar-brand pull-right" href="/secure/settings/#/settings">Settings</a>
 <a class="brand navbar-brand pull-right" href="/secure/docs/Getting_Started.html">Documentation</a>
